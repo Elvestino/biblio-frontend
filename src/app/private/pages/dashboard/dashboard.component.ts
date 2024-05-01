9f2b71c6fbc423cf30b7968f5aeceb46dfdca708
TSIANOY Karl Elvestino Dorelin <elvestinodorelin@gmail.com>
2024-05-01 11:32:04 +0300

    style : dashboard count

--- src/app/private/pages/dashboard/dashboard.component.ts
+++ src/app/private/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
+import { AdherentService } from '../../service/adherent.service';
+import { BibliothecaireService } from '../../service/bibliothecaire.service';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -7,28 +9,66 @@ import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-  title = 'ng2-charts-demo';
+export class DashboardComponent implements OnInit {
+  constructor(
+    private adherent: AdherentService,
+    private bibliothecaire: BibliothecaireService
+  ) {}
 
-  public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)',
+  // title = 'ng2-charts-demo';
+
+  // public lineChartData: ChartConfiguration<'line'>['data'] = {
+  //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  //   datasets: [
+  //     {
+  //       data: [65, 59, 80, 81, 56, 55, 40],
+  //       label: 'Series A',
+  //       fill: true,
+  //       tension: 0.5,
+  //       borderColor: 'black',
+  //       backgroundColor: 'rgba(255,0,0,0.3)',
+  //     },
+  //   ],
+  // };
+  // public lineChartOptions: ChartOptions<'line'> = {
+  //   responsive: false,
+  // };
+  // public lineChartLegend = true;
+
+  ngOnInit() {
+    this.getadherent();
+    this.getbibliothecaire();
+  }
+
+  // --------------adherent------------------
+  adherentdata: any[] = [];
+  getadherent() {
+    this.adherent.getAlladherents().subscribe({
+      next: (res) => {
+        this.adherentdata = res;
       },
-    ],
-  };
-  public lineChartOptions: ChartOptions<'line'> = {
-    responsive: false,
-  };
-  public lineChartLegend = true;
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+  adherenttotal(): number {
+    return this.adherentdata.length;
+  }
 
-  constructor() {}
-
-  ngOnInit() {}
+  // --------------bibliothecaire------------------
+  bibliothecairedata: any[] = [];
+  getbibliothecaire() {
+    this.bibliothecaire.getAllBibliothecaires().subscribe({
+      next: (res) => {
+        this.bibliothecairedata = res;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+  bibliothecairetotal(): number {
+    return this.bibliothecairedata.length;
+  }
 }
